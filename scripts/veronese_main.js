1aeb23e15392b0fa2e14f81e5815e9f80db032df
Robert Menger <robert.menger@tu-dresden.de>
2017-06-26 11:24:38 +0200

    dynamically load data from json in to carousel

--- scripts/veronese_main.js
+++ scripts/veronese_main.js
@@ -12,20 +12,86 @@ var jsonContent = null;
 
 function init() {
 	var carousel = document.getElementById("box_carousel");
-	if (!(data instanceof Array)) {
+	if (!(jsonContent instanceof Array)) {
 		alert("missing JSON data");
 		return;
 	}
-	maxSlides = data.length;
+	maxSlides = jsonContent.length; //start numbering of slides beginning from 1
 	currentSlide = 1;
 	for (var i=1; i<=maxSlides; i++) {
-		
+		var dataobject = null;
+		for (var j=0; j<maxSlides; j++) {
+			//retrieve element from json array
+			if (jsonContent[j].id == i) dataobject = jsonContent[j];
+		}
+		if (dataobject != null) createHTMLslide(carousel, dataobject);
 	}
 	redrawCarousel();
 }
 
 function returnJSONdata(data) {
 	jsonContent = data;
+}
+
+function createHTMLslide(carousel, dataobject) {
+		/*
+slideContainer	<div id="Slide03" class="carousel carousel_visible">
+titleDiv			<div class="carousel_title_active"><p>Ich bin ein Titel</p></div>
+contentDiv			<div class="carousel_content_active">
+contentInfoDiv			<div class="info_content">
+contentInfoTitle			<h1>Ich bin ein Titel</h1>
+contentInfoContent			<div><p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+							<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos </p>
+							</div>
+						</div>
+pictureDiv				<div class="picture_slider">
+							<img src="images/mockup1.jpg" class="thumbnail" />
+							<img src="images/mockup1.jpg" class="thumbnail" />
+							<img src="images/mockup1.jpg" class="thumbnail" />
+							<img src="images/mockup1.jpg" class="thumbnail" />
+							<img src="images/mockup1.jpg" class="thumbnail" />
+						</div>
+					</div>
+				</div>
+		*/
+	var slideContainer = document.createElement("div");
+	var number = ("0" + dataobject.id).slice(-2); //padding of a leading zero
+	slideContainer.id = "Slide" + number;
+	
+	var titleDiv = document.createElement("div");
+	var titleP = document.createElement("p");
+	titleP.textContent = dataobject.title;
+	
+	var contentDiv = document.createElement("div");
+	var contentInfoDiv = document.createElement("div");
+	contentInfoDiv.className = "info_content";
+	var contentInfoTitle = document.createElement("h1");
+	contentInfoTitle.textContent = dataobject.title;
+	var contentInfoContent = document.createElement("div");
+	contentInfoContent.innerHTML = $.ajax({
+		type: "GET",
+		url: "content/" + dataobject.content,
+		async: false
+	}).responseText;
+	
+	var pictureDiv = document.createElement("div");
+	pictureDiv.className = "picture_slider";
+	for (var i=0; i<dataobject.images.length; i++) {
+		var imageElement = document.createElement("img");
+		imageElement.src = "images/" + dataobject.images[i];
+		imageElement.className = "thumbnail";
+		pictureDiv.appendChild(imageElement);
+	}
+	
+	carousel.appendChild(slideContainer);
+	slideContainer.appendChild(titleDiv);
+	titleDiv.appendChild(titleP);
+	slideContainer.appendChild(contentDiv);
+	contentDiv.appendChild(contentInfoDiv);
+	contentInfoDiv.appendChild(contentInfoTitle);
+	contentInfoDiv.appendChild(contentInfoContent);
+	
+	contentDiv.appendChild(pictureDiv);
 }
 
 
