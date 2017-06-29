3c82173f8b35e35d1ed49e657ab5deb9bedcfe44
Robert mneger <mail@poisonnuke.de>
2017-06-29 21:23:40 +0200

    new slideshow, overview image

--- scripts/veronese_main.js
+++ scripts/veronese_main.js
@@ -11,7 +11,8 @@ var jsonContent = null;
 var currentImage = 0;
 var maxImages = 1;
 var slideshowContainer = null;
-var slideShowActive = false;
+var overviewContainer = null;
+var overviewEnlarged = false;
 
 function init() {
 	if (!initialized) {
@@ -30,6 +31,11 @@ function prepareCarousel() {
 		console.log("JSON CANNOT BE LOADED, XMLhttp request blocked?");
 		return;
 	}
+	overviewContainer = document.createElement("div");
+	overviewContainer.id = "overview_small";
+	overviewContainer.onclick = toggleOverview;
+	document.body.appendChild(overviewContainer);
+	
 	var carousel = document.getElementById("box_carousel");
 	maxSlides = jsonContent.length; //start numbering of slides beginning from 1
 	currentSlide = 1;
@@ -116,13 +122,20 @@ contentInfoContent			<div><p>Lorem ipsum dolor sit amet, consetetur sadipscing e
 	pictureWrapper.appendChild(pictureDiv);
 	contentDiv.appendChild(contentInfoDiv);
 	contentInfoDiv.appendChild(contentInfoContent);
+	
+	var overviewImage = document.createElement("img");
+	overviewImage.src = "images/overview/" + dataobject.overview;
+	overviewImage.alt = "Gesamtansicht des Werkes";
+	overviewImage.id = "overview"+("0" + dataobject.id).slice(-2);
+	overviewImage.className = "overview_image_inactive";
+	overviewContainer.appendChild(overviewImage);
 }
-
 
 function redrawCarousel() {
 	for (var i=1; i <= maxSlides; i++) {
 		var number = ("0" + i).slice(-2); //padding of a leading zero
 		var slide = document.getElementById("Slide"+number);
+		var overviewImage = document.getElementById("overview"+number);
 		var containerCSS = "";
 		var childTitleCSS = "";
 		var childContentCSS ="";
@@ -139,17 +152,20 @@ function redrawCarousel() {
 				containerCSS = "carousel_visible";
 				childTitleCSS = "carousel_title_hidden";
 				childContentCSS = "carousel_content_active";
+				overviewImage.className = "overview_image_active";
 				break;
 			case SLIDESTATES.MINIMIZED:
 				containerCSS = "carousel_minimized";
 				childTitleCSS = "carousel_title_min";
 				childContentCSS = "carousel_content_hidden";
+				overviewImage.className = "overview_image_inactive";
 				onClickAction = (i < currentSlide) ? prevSlide : nextSlide;
 				break;
 			case SLIDESTATES.HIDDEN:
 				containerCSS = "carousel_hidden";
 				childTitleCSS = "carousel_title_hidden";
 				childContentCSS = "carousel_content_hidden";
+				overviewImage.className = "overview_image_inactive";
 				break;
 		}
 		containerCSS += " carousel";
@@ -168,6 +184,18 @@ function redrawCarousel() {
 	redrawSlideshow();
 }
 
+//move carousel to corresponding slide, if timeline-slider is moved
+function updateCarousel(activeSlides) {
+	//simple version, show the first item in the list. If List is empty, do not change anything
+	if (!initialized) return;
+	if (!(activeSlides instanceof Array)) return;
+	if (activeSlides.length == 0) return;
+	if (currentSlide != activeSlides[0]) {
+		currentSlide = activeSlides[0];
+		redrawCarousel();
+	}
+}
+
 function nextSlide() {
 	if (currentSlide < maxSlides) {
 		currentSlide++;
@@ -183,18 +211,7 @@ function prevSlide() {
 	}
 }
 
-
-function updateCarousel(activeSlides) {
-	//simple version, show the first item in the list. If List is empty, do not change anything
-	if (!initialized) return;
-	if (!(activeSlides instanceof Array)) return;
-	if (activeSlides.length == 0) return;
-	if (currentSlide != activeSlides[0]) {
-		currentSlide = activeSlides[0];
-		redrawCarousel();
-	}
-}
-
+//picture slideshow inside the carousel, move images
 function redrawSlideshow() {
 	for (var i = 0; i<maxImages; i++) {
 		var imageElement = slideshowContainer.children[i];
@@ -222,15 +239,32 @@ function prevImage() {
 	redrawSlideshow();
 }
 
+function toggleOverview() {
+	var maindiv = document.getElementById("maindiv");
+	if (overviewEnlarged) {
+		overviewContainer.id = "overview_small";
+		overviewEnlarged = false;
+		maindiv.onclick = function() { return false; };
+	} else {
+		overviewContainer.id = "overview_large";
+		overviewEnlarged = true;
+		maindiv.onclick = toggleOverview;
+	}
+}
+
+//only for debugging, shall be replaced by gesture or buttons etc
 $(document).keydown(function(e) {
 	//this function rotates the carousel
 	switch(e.which) {
+		case 27: //es
+			if (overviewEnlarged) toggleOverview();
+			break;
 		case 37: // left
 			prevSlide();
-		break;
+			break;
 		case 39: // right
 			nextSlide()
-		break;
+			break;
 		default: return; // exit this handler for other keys
 	}
 	e.preventDefault(); // prevent the default action (scroll / move caret)
